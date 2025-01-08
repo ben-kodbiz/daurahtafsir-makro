7787986f370ec794bbb6ff262093db83bfd60ba4
ben <ben@test.com>
2025-01-08 12:04:44 +0800

    updateHtmlhttps

--- qawaid_tafsir/change_pdf.sh
+++ qawaid_tafsir/change_pdf.sh
@@ -6,11 +6,12 @@ for i in $(seq 1 115); do
 
   # Check if the file exists
   if [ -f "$filename" ]; then
-    # Update the iframe src attribute
-    sed -i -E 's#(<iframe[^>]*src=")(\.\./pdfs/)([^"]+)#\1https://ben-kodbiz.github.io/daurahtafsir-makro/pdfs/\3#' "$filename"
+    # Ensure existing http paths are converted to https
+    sed -i -E 's#http://kodbiz.github.io/daurahtafsir-makro/pdfs/#https://kodbiz.github.io/daurahtafsir-makro/pdfs/#g' "$filename"
 
-    # Update the anchor href attribute
-    sed -i -E 's#(<a[^>]*href=")(\.\./pdfs/)([^"]+)#\1https://ben-kodbiz.github.io/daurahtafsir-makro/pdfs/\3#' "$filename"
+    # Append https:// URL to relative paths
+    sed -i -E 's#(<iframe[^>]*src=")(\.\./pdfs/)([^"]+)#\1https://kodbiz.github.io/daurahtafsir-makro/pdfs/\3#' "$filename"
+    sed -i -E 's#(<a[^>]*href=")(\.\./pdfs/)([^"]+)#\1https://kodbiz.github.io/daurahtafsir-makro/pdfs/\3#' "$filename"
 
     echo "Updated file: $filename"
   else

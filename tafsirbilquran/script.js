3137fe60a7bab86d2b40e77a1271ad54a87ed479
ben <ben@test.com>
2025-01-13 22:43:23 +0800

    AddedNewapps

--- tafsirbilquran/script.js
+++ tafsirbilquran/script.js
@@ -1,28 +1,37 @@
 function createSessionGrid() {
-    const sessionGrid = document.getElementById("session-grid");
-    const sessionFiles = [];
-    for (let i = 1; i <= 80; i++) {
-        sessionFiles.push(`session_${i}.html`);
-    }
+    fetch("../data/tafsirquranbilquran.json")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const sessionGrid = document.getElementById("session-grid");
+            const sessions = data.sessions;
+            sessions.forEach(session => {
+                const sessionDiv = document.createElement("div");
+                 sessionDiv.classList.add("session-item");
+                 sessionDiv.addEventListener("click", () => {
+                     window.location.href = `session_${session.session_number}.html`;
+                });
 
-    sessionFiles.forEach(filename => {
-        const sessionNumber = filename.match(/session_(\d+)\.html/)[1];
-        const sessionDiv = document.createElement("div");
-        sessionDiv.classList.add("session-item");
-        sessionDiv.addEventListener("click", () => {
-            window.location.href = filename;
+                const sessionNumber = document.createElement("span");
+                sessionNumber.classList.add("session-number");
+                sessionNumber.textContent = session.session_number;
+
+                const sessionTitle = document.createElement("span");
+                sessionTitle.classList.add("session-title");
+                sessionTitle.textContent = session.title;
+
+                sessionDiv.appendChild(sessionNumber);
+                sessionDiv.appendChild(sessionTitle);
+                sessionGrid.appendChild(sessionDiv);
+            });
+
+        })
+        .catch(error => {
+             console.error("Error loading or processing data:", error);
         });
-        const sessionNumberSpan = document.createElement("span");
-        sessionNumberSpan.classList.add("session-number");
-        sessionNumberSpan.textContent = sessionNumber;
-
-         const sessionTitleSpan = document.createElement("span");
-        sessionTitleSpan.classList.add("session-title");
-        sessionTitleSpan.textContent = `Session ${sessionNumber}`;
-
-        sessionDiv.appendChild(sessionNumberSpan);
-        sessionDiv.appendChild(sessionTitleSpan);
-        sessionGrid.appendChild(sessionDiv);
-    });
 }
 createSessionGrid();
